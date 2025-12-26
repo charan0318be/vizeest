db1be05205c7b9fb106a0658b656410653d1ad12
pd1-lgtm <pd1@zenitude.ai>
2025-12-26 09:13:37 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,16 +1,6 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  // DEVELOPMENT MODE: Disallow all crawling
-  // TODO: Remove this block and uncomment production rules when ready to launch
-  return {
-    rules: {
-      userAgent: '*',
-      disallow: '/',
-    },
-  };
-
-  /* PRODUCTION RULES - Uncomment when ready to launch
   return {
     rules: [
       {
@@ -32,5 +22,4 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: 'https://vizeest.com/sitemap.xml',
     host: 'https://vizeest.com',
   };
-  */
 }
