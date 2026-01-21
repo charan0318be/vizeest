cacc3c01259e44e8d959ce215a82d41442dbb924
pd1-lgtm <pd1@zenitude.ai>
2026-01-21 11:21:27 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://vizeest.com',
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://stag-api.vizeest.us',
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-8JYZRSGE9T',
   },
 };
 
